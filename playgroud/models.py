c25381436b3aee0c96285460d739fcf88e537f33
Louis <lyeqing@ymail.com>
2024-06-08 18:18:50 +0930

    added two tables

--- playgroud/models.py
+++ playgroud/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+class Persons(models.Model):
+    person_id = models.AutoField(primary_key=True)
+    mobile = models.CharField(max_length=20, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField()
+    address = models.CharField(max_length=255, null=True, blank=True)
+    suburb = models.CharField(max_length=100, null=True, blank=True)
+    postcode = models.CharField(max_length=10, null=True, blank=True)
+    comment = models.TextField(null=True, blank=True)
+    password = models.CharField(max_length=128)
+    token = models.UUIDField(default=uuid.uuid4, null=True, blank=True)
+
+
+class Activities(models.Model):
+    activity_id = models.AutoField(primary_key=True)
+    person = models.ForeignKey('Persons', on_delete=models.CASCADE, null=False, blank=False)
+    category = models.CharField(max_length=100, null=True, blank=True)
+    note = models.TextField(null=True, blank=True)
+    log_time = models.DateTimeField()
